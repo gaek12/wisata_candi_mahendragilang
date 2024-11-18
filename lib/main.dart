9fe9a406d8c256a91896aa2c1a2be70a816d6770
se <160828231+gaek12@users.noreply.github.com>
2024-11-18 12:22:34 +0700

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
+import 'package:wisata_candi/screens/favorite_screen.dart';
 import 'package:wisata_candi/screens/home_screen.dart';
+import 'package:wisata_candi/screens/profile_screen.dart';
+import 'package:wisata_candi/screens/search_screen.dart';
+import 'package:wisata_candi/screens/sign_in_screen.dart';
+import 'package:wisata_candi/screens/sign_up_screen.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Wisata Candi',
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
@@ -31,11 +38,70 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      // home: const ProfileScreen(),
-      // home: DetailScreen(candi: candiList[0]),
-      // home: SignInScreen(),
-      //home: SearchScreen(),
-      home: HomeScreen(),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen ({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  // TODO: 1. Deklarasikan variabel
+  int _currentIndex = 0;
+
+  final List<Widget> _children = [
+    const HomeScreen(),
+    const SearchScreen(),
+    const FavoriteScreen(),
+    const ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // TODO: 2. Buat property body berupa widget yang ditampilkan
+      body:  _children[_currentIndex],
+      // TODO: 3. Buat property bottomNavigationBar dengan nilai Theme
+      bottomNavigationBar: Theme(
+        // TODO: 4. Buat data dan child dan Theme
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.deepPurple[50],
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: Colors.deepPurple,),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search, color: Colors.deepPurple,),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite, color: Colors.deepPurple,),
+              label: 'Favorite',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person, color: Colors.deepPurple,),
+              label: 'Profile',
+            ),
+          ],
+          selectedItemColor: Colors.deepPurple,
+          unselectedItemColor: Colors.deepPurple[100],
+          showUnselectedLabels: true,
+        ),
+      ),
     );
   }
 }
--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -1,80 +1,80 @@
 import 'package:flutter/material.dart';
 
-class SignInScreen extends StatefulWidget {
-  SignInScreen({super.key});
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
 
   @override
-  State<SignInScreen> createState() => _SignInScreenState();
+  State<SignUpScreen> createState() => _SignUpScreenState();
 }
 
-class _SignInScreenState extends State<SignInScreen> {
-
-  // TODO : 1. Deklarasikan variabel
+class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController _nameController = TextEditingController();
   final TextEditingController _usernameController = TextEditingController();
-
   final TextEditingController _passwordController = TextEditingController();
 
-  String _errorText = '';
-  bool _isSignedIn = false;
   bool _obscurePassword = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // TODO : 2. Pasang AppBar
-      appBar: AppBar(title: Text('Sign In'),),
-      // TODO : 3. Pasang body
+      appBar: AppBar(title: Text('Sign Up')),
       body: Center(
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(16),
-            child: Form(
-              child: Column(
-                // TODO : 4. Atur mainAxisAlignment dan crossAxisAlignment
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  // TODO : 5. Pasang TextFormField Nama pengguna
-                  TextFormField(
-                    controller: _usernameController,
-                    decoration: InputDecoration(
-                      labelText: "Nama Pengguna",
-                      border: OutlineInputBorder(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // TextFormField for Nama
+                TextFormField(
+                  controller: _nameController,
+                  decoration: InputDecoration(
+                    labelText: "Nama",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20),
+
+                // TextFormField for Nama Pengguna
+                TextFormField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(
+                    labelText: "Nama Pengguna",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20),
+
+                // TextFormField for Kata Sandi
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    labelText: "Kata Sandi",
+                    border: OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _obscurePassword = !_obscurePassword;
+                        });
+                      },
+                      icon: Icon(
+                        _obscurePassword ? Icons.visibility_off : Icons.visibility,
+                      ),
                     ),
                   ),
-                  // TODO : 6. Pasang TextFormField Kata Sandi
-                  SizedBox(height: 20),
-                  TextFormField(
-                    controller: _passwordController,
-                    decoration: InputDecoration(
-                      labelText: "Kata Sandi",
-                      errorText: _errorText.isNotEmpty ? _errorText : null,
-                      border: OutlineInputBorder(),
-                      suffixIcon: IconButton(
-                        onPressed: (){
-                          setState(() {
-                            _obscurePassword = !_obscurePassword;
-                          });
-                        },
-                        icon: Icon(
-                          _obscurePassword ? Icons.visibility_off
-                              : Icons.visibility,
-                        ),),
-                    ),
-                    obscureText: _obscurePassword,
-                  ),
-                  // TODO : 7. Pasang ElevatedButton SignIn
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                      onPressed: (){},
-                      child: Text('Sign In')),
-                  // TODO : 8. Pasang TextButton Sign Up
-                  SizedBox(height: 10),
-                  TextButton(
-                      onPressed: (){},
-                      child: Text('Belum punya akun? Daftar di sini.')),
-                ],
-              ),
+                  obscureText: _obscurePassword,
+                ),
+                SizedBox(height: 28),
+
+                // ElevatedButton for Sign Up
+                ElevatedButton(
+                  onPressed: () {
+                    // TODO: Implement sign up functionality
+                  },
+                  child: Text('Sign Up'),
+                ),
+              ],
             ),
           ),
         ),
